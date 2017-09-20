54a4942588fb6ca6ae4df4a71eac61512fe25683
IsLiHong <1606059227@qq.com>
2017-09-20 11:09:21 +0800

    测试是否成功

---
+++ src/com/lh/test/Test.java
@@ -0,0 +1,12 @@
+package com.lh.test;
+
+/**
+ * @version :1.0
+ * CREATE TIME :2017/9/20 11:07
+ * @authro :LH
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
